4a71e5f312c76f6c4ea5d653de71256865a54174
Robert Szasz <robert_szasz-bacso@trimble.com>
2025-03-31 12:33:07 +0300

    SCRUM-33: Added endpoints for upserting, getting and deleting grades

--- Repositories/GradeRepository.cs
+++ Repositories/GradeRepository.cs
@@ -3,6 +3,7 @@ namespace CatalogOnline_API.Repositories;
 using CatalogOnline_API.Interfaces.Repositories;
 using CatalogOnline_API.Models;
 using MongoDB.Driver;
+using System.Threading.Tasks;
 
 public class GradeRepository : IGradeRepository
 {
@@ -18,4 +19,28 @@ public class GradeRepository : IGradeRepository
         return await _grades.Find(g => g.StudentId == studentId && g.ScienceId == scienceId).ToListAsync();
     }
 
+    public async Task<Grade> GetGradeByIdAsync(string id)
+    {
+        return await _grades.Find(g => g.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task UpsertGradeAsync(Grade grade)
+    {
+        if (string.IsNullOrEmpty(grade.Id))
+        {
+            grade.Id = Guid.NewGuid().ToString();
+            await _grades.InsertOneAsync(grade);
+        }
+        else
+        {
+            var filter = Builders<Grade>.Filter.Eq(g => g.Id, grade.Id);
+            await _grades.ReplaceOneAsync(filter, grade, new ReplaceOptions { IsUpsert = true });
+        }
+    }
+
+    public async Task<bool> DeleteGradeAsync(string id)
+    {
+        var result = await _grades.DeleteOneAsync(g => g.Id == id);
+        return result.DeletedCount > 0;
+    }
 }
